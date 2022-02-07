a760019c61ba932c4b8c748bd007cfacfe763cdf
Fraanlnc <97235905+Fraanlnc@users.noreply.github.com>
2022-02-07 11:12:36 -0300

    Add files via upload

---
+++ TP2/asd.bat
@@ -0,0 +1,7 @@
+echo "# Parcial-II" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Fraanlnc/Parcial-II.git
+git push -u origin main
